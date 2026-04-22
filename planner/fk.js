e3d78247a357140407e2c88b5c1307ef8cca49a4
Sumanth2203 <140178867+Sumanth2203@users.noreply.github.com>
2026-04-23 01:49:26 +0530

    Update fk.js

--- planner/fk.js
+++ planner/fk.js
@@ -1,6 +1,27 @@
+const robot = require("./urdf_parser");
+
+function rad(d){ return d*Math.PI/180; }
+
 function fk(j){
-  return {
-    tcp:[391,0,3040]
-  };
+
+  const j1 = rad(j[0]);
+  const j2 = rad(j[1]);
+  const j3 = rad(j[2]);
+
+  const r =
+    robot.xFixed +
+    robot.L2*Math.cos(j2) +
+    robot.L3*Math.cos(j2+j3);
+
+  const z =
+    robot.baseZ -
+    robot.L2*Math.sin(j2) -
+    robot.L3*Math.sin(j2+j3);
+
+  const x = r*Math.cos(j1);
+  const y = r*Math.sin(j1);
+
+  return [x,y,z];
 }
+
 module.exports = fk;
