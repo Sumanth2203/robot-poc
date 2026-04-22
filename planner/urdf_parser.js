a7e30003c00a3e9d5c7584c7d7f5c54fef2ac946
Sumanth2203 <140178867+Sumanth2203@users.noreply.github.com>
2026-04-23 01:40:05 +0530

    Create urdf_parser.js

---
+++ planner/urdf_parser.js
@@ -0,0 +1,11 @@
+module.exports = {
+  joints: [
+    {name:"A1", origin:[0,0,675], axis:[0,0,-1]},
+    {name:"A2", origin:[350,0,0], axis:[0,1,0]},
+    {name:"A3", origin:[1150,0,0], axis:[0,1,0]},
+    {name:"A4", origin:[1000,0,-41], axis:[-1,0,0]},
+    {name:"A5", origin:[0,0,0], axis:[0,1,0]},
+    {name:"A6", origin:[0,0,0], axis:[-1,0,0]}
+  ],
+  tool0:[215,0,0]
+};
