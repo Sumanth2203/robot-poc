f701ff4dbb05f54b741f22c6af4bdb8ee09b0c7e
Sumanth2203 <140178867+Sumanth2203@users.noreply.github.com>
2026-04-23 01:41:36 +0530

    Add linkBoxes function to create box configurations

---
+++ planner/link_boxes.js
@@ -0,0 +1,9 @@
+function linkBoxes(joints){
+
+  return [
+    {name:"link1", center:[0,0,500], size:[400,400,1000]},
+    {name:"link2", center:[600,0,1200], size:[1200,250,250]},
+    {name:"link3", center:[1500,0,1800], size:[1000,220,220]}
+  ];
+}
+module.exports = linkBoxes;
