69af5a0054b87b9ce3b289fd3054bb42b20c3015
Sumanth2203 <140178867+Sumanth2203@users.noreply.github.com>
2026-04-23 01:44:57 +0530

    Add additional link boxes with updated sizes and positions

--- planner/link_boxes.js
+++ planner/link_boxes.js
@@ -1,9 +1,14 @@
 function linkBoxes(joints){
 
   return [
-    {name:"link1", center:[0,0,500], size:[400,400,1000]},
-    {name:"link2", center:[600,0,1200], size:[1200,250,250]},
-    {name:"link3", center:[1500,0,1800], size:[1000,220,220]}
+    {name:"base",  center:[0,0,350],   size:[800,800,700]},
+    {name:"link1", center:[0,0,900],   size:[500,500,600]},
+    {name:"link2", center:[600,0,1200],size:[1200,260,260]},
+    {name:"link3", center:[1500,0,1800],size:[1000,240,240]},
+    {name:"link4", center:[2100,0,1800],size:[350,350,350]},
+    {name:"link5", center:[2300,0,1800],size:[280,280,280]},
+    {name:"link6", center:[2500,0,1800],size:[300,220,220]}
   ];
+
 }
 module.exports = linkBoxes;
